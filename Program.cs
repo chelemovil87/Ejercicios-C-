72e3e24b574057a622379178de5edab82e9bd69f
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2023-01-31 12:43:39 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        client Antonio = new client("Antonio Jurado", 438483290, "c/garcia lorca 2", "antonioJurado@hotmail.es", true);
+        Console.WriteLine(Antonio);
+        public struct client
+        {
+            public client (string name, int tlf, string direction, string email, bool newClient)
+            {
+                name = "Jose Rodriguez";
+                tlf = 843487190;
+                direction = "c/julio romero 21";
+                email = "jose@gmail.com";
+                newClient = false;
+            }
+
+            public string name { get; set; }
+            public int tlf { get; set; }
+            public string direction { get; set; }
+            public string email { get; set; }
+            public bool newClient { get; set; }
+
+
+            public override string ToString() => $"({name}, {tlf}, {direction}, {email}, {newClient})";
+            
+            
+        }
+
+        
+    }
+}
